24ac1865a49eade2ba5d801c5957bd670ac1d273
Daniel C <104099162+CatX711@users.noreply.github.com>
2022-06-10 22:31:06 +0100

    fixed unsafe code

--- Converting_input_into_int.py
+++ Converting_input_into_int.py
@@ -1,16 +1,14 @@
 # What is an integer in Python? An integer is basically a number, and a string is text. When you type a number into a variable, the language assumes that it is a string,
 # but to not have this, we have to convert the input into an integer.
-# What you do is you type "int" and in open and closed brackets, type your variable that equals an input.
+# What you do is you type "int" and in open and closed brackets, type input(your text).
 # Your program would look like this:
 
-marmalade = input("Type a number: ")
-int(input())
+marmalade = int(input("Type a number: "))
 
 # This converts the input, for example, 5, from string, which is what the computer assumes it is, into an interger.
 # Then you can get the program to print the input x2. To do this, with your old code, write, "print(int(input()) * 2)" (times in Python is an asteric) 
 
-marmalade = input("Type a number: ")
-int(input())
+marmalade = int(input("Type a number: "))
 print(int(input()) * 2)
 
 # Your result should be the program asking you for a number, and then taking that number, realising it is a number, and not text, and multiplying it by two.
@@ -21,7 +19,7 @@ print(int(input()) * 2)
 # We can expand upon this, by adding text into our final print line.
 # In Python, different things can be seperated by a comma, so if you wanted to add text to a print function that repeats your name, it wuld look like this:
 
-name = input("Type a name: ")
+name = int(input("Type a name: "))
 print("Your name is", input(), "!")
 
 # You should get this:
@@ -30,7 +28,7 @@ print("Your name is", input(), "!")
 
 # Now do this to your code, eg:
 
-print("Your number multiplied by two is: ", marmalade)
+print("Your number multiplied by two is: ", marmalade * 2)
 
 # You should get this as your response:
 # Type a number: 22
@@ -38,6 +36,5 @@ print("Your number multiplied by two is: ", marmalade)
 
 # Full code:
 
-marmalade = input("Type a number: ")
-int(input())
-print("Your number multiplied by two is:", marmalade)
+marmalade = int(input("Type a number: "))
+print("Your number multiplied by two is:", marmalade * 2)
