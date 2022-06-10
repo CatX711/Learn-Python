aed8e501ae959b85b0847638001692f629cb3786
Daniel C <104099162+CatX711@users.noreply.github.com>
2022-06-10 21:36:00 +0100

    Update Basics.py

--- Basics.py
+++ Basics.py
@@ -1,1 +1,12 @@
+# Python is a very complex and powerful programming language, but that does not necessarily mean that it is hard to understand, in fact, as soon as you open Python,
+# you can get started straight away!
 
+# Download: https://www.python.org/
+
+# Once you open the idle shell, you can create your first code.
+
+print("Hello, world!")
+
+# The print function in python tells the computer to display something on the screen. That something is what you choose it to say, like for example, the famous, 'hello world'
+# Give it a try yourself, type, 'print' (always in lowercase) and then opened and closed brackets, and inside those brackets, two inverted commas, and inside those your text.
+# When run, the computer will say whatever you tell it to do (just don't make it say rude words like 'bottoms', please, I once did that and my grandma fainted at the sight!
