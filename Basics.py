aa0af292970311368aaa411e09dbeb53073c80c4
Daniel C <104099162+CatX711@users.noreply.github.com>
2022-06-10 21:40:47 +0100

    Update Basics.py

--- Basics.py
+++ Basics.py
@@ -10,3 +10,36 @@ print("Hello, world!")
 # The print function in python tells the computer to display something on the screen. That something is what you choose it to say, like for example, the famous, 'hello world'
 # Give it a try yourself, type, 'print' (always in lowercase) and then opened and closed brackets, and inside those brackets, two inverted commas, and inside those your text.
 # When run, the computer will say whatever you tell it to do (just don't make it say rude words like 'bottoms', please, I once did that and my grandma fainted at the sight!
+
+# Now you can print whatever you want onto the screen. Why not mess around with it? Have some fun and practice the function?
+
+print("I like tea, but in the mornings, I preffer coffee.")
+print("""
+
+    Becky's beagle barked and bayed, becoming bothersome for Billy.
+    Can you keep the cat from clawing the couch? It's creating chaos.
+    Dan's dog dove deep in the dam, drinking dirty water as he dove.
+    Fred's friends fried fritters for Friday's food.
+    Greedy goats gobbled up gooseberries, getting good at grabbing the goodies.
+    Hannah's home has heat now, hopefully.
+    Jackrabbits jump and jiggle jauntily.
+    Kim's kid kept kicking like crazy.
+    Larry's lizard likes lounging on the land.
+    Mike made mellow music with his new microphone.
+    Nick's nephew needed some new notebooks.
+    Peter's piglet pranced priggishly.
+    Quincy's quilters quit quilting quickly.
+    Rudolph the red-nosed reindeer rose rapidly into the air.
+    Seven sisters slept soundly on the sand.
+    Tim took tons of tools to make toys for the tots.
+    Vivien is very vixen-like and vexing.
+    While walking wearily I wondered where Wally was.
+    Yarvis yanked his ankle at yoga, and Yolanda yelled out in surprise.
+    Zachary zeroed in on zookeeping as a career.
+    
+    """)
+# You can also use six inverted commas and in the middle, your text. You can write as many lines as you want, until you stop. As seen above.
+
+print("bottoms")
+
+# Oh no...! I think I'm going to f-
