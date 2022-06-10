c4faece9aa384a9fa8821a5293383144ce6d6dc2
Daniel C <104099162+CatX711@users.noreply.github.com>
2022-06-10 21:58:32 +0100

    Create Inputs_and_variables.py

---
+++ Inputs_and_variables.py
@@ -0,0 +1,26 @@
+# What is an input? What is a variable? All of those questions will be answered now.
+
+# An input is essentially a way to give the computer some numbers of text, and it will do things with that.
+# Think of a variable as a box. You can assign names to this box, and this box can store things inside it, like text or numbers.
+
+# Here is an example using both of them:
+
+# The variable is "answer" and the input prints out a message asking you for your name. The reason why you leave a space after the last character before the inverted commas is
+# because when the question is asked, you would be typing it right next to the question mark. Let's say you were called Dan. If you didn't leave a space, it would look like this:
+# What is your name?Dan
+# If we leave a space, instead, it looks like this:
+# What is your name? Dan
+
+answer = input("What is your name? ")
+
+# The reason why you leave a space after the last character before the inverted commas is
+# because when the question is asked, you would be typing it right next to the question mark. Let's say you were called Dan. If you didn't leave a space, it would look like this:
+# What is your name?Dan
+# If we leave a space, instead, it looks like this:
+# What is your name? Dan
+
+# The variable doesn't have to be called answer though it could be anything you want. It could be 'ice cream' or 'spam', 'eggs', or, my personal favourite, 'smelly_socks'
+# TIP : If you have two words in your variable name, don't use a space to seperate them, use an underscore. Like this: Spam_and_eggs
+
+
+
