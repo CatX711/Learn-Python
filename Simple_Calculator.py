2593c1ea7f11491625366fbb3d08166807fc345c
Daniel C <104099162+CatX711@users.noreply.github.com>
2022-06-11 10:31:27 +0100

    Create Simple_Calculator.py

---
+++ Simple_Calculator.py
@@ -0,0 +1,16 @@
+# If you thought that making a calculator in Python is probably extremely hard, you're wrong.
+# You have all the tools at your disposal to make one. A simple calculator that adds up the two numbers that you input can be made in 2 minutes or less.
+# Let's give it a try.
+# First, create a "number1" variable, and a "number2" variable, that both equal some input:
+
+number1 = input("Enter your first number: ")
+number2 = input("Enter your second number: ")
+
+# (You can have anything inside the input("") that you want.)
+
+# Now, add int and brackets around the two inputs for the two variables, like this:
+
+number1 = int(input("Enter your first number: "))
+number2 = int(input("Enter your second number: "))
+
+# This will convert whatever number you type from string into an integer.
