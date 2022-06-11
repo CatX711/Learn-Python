4bb3a756d379e9a8c52423be3df6c66ace564caa
Daniel C <104099162+CatX711@users.noreply.github.com>
2022-06-11 14:53:37 +0100

    Create Len.py

---
+++ Len.py
@@ -0,0 +1,6 @@
+# So, now that you have created your first calculator, we will look at other functions. One of them is called "len".
+# What len does is take an input and count the number of characters in that input. I have created my own version of len (in list of functions that I have made).
+# How do you use len?
+# Well, considering you now know inputs and variables, lets combine len with them to make a program that counts the characters in what you input.
+
+sentence = input("Enter a string. It can be as long or as short as you want (reminder: strings are collections of words or letters): ")
