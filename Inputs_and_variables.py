57c7cd8e6dc017dc37c182bb800e578f57155646
Daniel C <104099162+CatX711@users.noreply.github.com>
2022-06-11 18:45:40 +0100

    Update Inputs_and_variables.py

--- Inputs_and_variables.py
+++ Inputs_and_variables.py
@@ -13,11 +13,5 @@
 
 answer = input("What is your name? ")
 
-# The reason why you leave a space after the last character before the inverted commas is
-# because when the question is asked, you would be typing it right next to the question mark. Let's say you were called Dan. If you didn't leave a space, it would look like this:
-# What is your name?Dan
-# If we leave a space, instead, it looks like this:
-# What is your name? Dan
-
 # The variable doesn't have to be called answer though it could be anything you want. It could be 'ice cream' or 'spam', 'eggs', or, my personal favourite, 'smelly_socks'
 # TIP : If you have two words in your variable name, don't use a space to seperate them, use an underscore. Like this: Spam_and_eggs
