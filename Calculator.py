34badf6232ef707c69283563152aa2d5e00941bd
Daniel C <104099162+CatX711@users.noreply.github.com>
2022-06-11 10:43:17 +0100

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,10 @@
+# Things are getting real now. In this, you will make the calculator divide, subtract, add and multiply. And you get to choose which one you want it to do.
+
+# Start off by creating a variable with a name called "selection", which equals and input.
+
+selection = input("Select what to do (add/multiply/divide/subtract")
+
+# Then type if selection.lower() == "add":
+# There's a lot to unpack there, so I'll go over it now 
+                  
+                  
