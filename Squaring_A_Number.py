3000f5a88471f2aae505a4b3cb13c207e3a7e7d2
Daniel C <104099162+CatX711@users.noreply.github.com>
2022-06-12 12:40:17 +0100

    Create Squaring_A_Number.py

---
+++ Squaring_A_Number.py
@@ -0,0 +1,10 @@
+# (this is a personal file so it wont be like a tutorial. For personal files that I myself can look at and read without the in depth writing my other tutorial files, there will
+# be a disclaimer, like this one)
+
+
+# Squaring a number is easy
+
+# take one input, convert it into int and times it by its self:
+
+number = int(input("Enter a number"))
+print(number * number)
