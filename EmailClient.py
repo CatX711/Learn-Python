2773b0880fa6cd6a027c657c3d6c8f6f08fca44b
Daniel C <104099162+CatX711@users.noreply.github.com>
2022-06-14 18:19:10 +0100

    added disclaimer

--- EmailClient.py
+++ EmailClient.py
@@ -1,3 +1,6 @@
+# (this is a personal file so it wont be like a tutorial. For personal files that I myself can look at and read without the in depth writing my other tutorial files, there will
+# be a disclaimer, like this one)
+
 # An email client using classes
 
 while True:
